c1ffe7eaeabd51cc438951c54042f8b321d3fdc3
Greg Leuch <contact@gleuch.com>
2011-01-10 02:08:57 -0500

    installing jquery-rails. removing unnecessary paperclip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,6 @@ class ApplicationController < ActionController::Base
   helper :all
   layout 'application'
 
-  filter_parameter_logging :password, :password_confirmation
-  helper_method :current_user_session, :current_user
-
-
   before_filter :app_init
 
 
--- config/initializers/paperclip_interpolations.rb
+++ config/initializers/paperclip_interpolations.rb
@@ -1,17 +1,17 @@
 
 # Switch to id partitioning for "new" paperclip uploads, so we don't have to move old ones (yet)
 # DEPRECATEME!
-Paperclip.interpolates(:id_smart) do |attachment, style|
-  limit = 33600
-  if attachment.instance.id > limit
-    id_partition(attachment, style)
-  else
-    id(attachment, style)
-  end    
-end
+# Paperclip.interpolates(:id_smart) do |attachment, style|
+#   limit = 33600
+#   if attachment.instance.id > limit
+#     id_partition(attachment, style)
+#   else
+#     id(attachment, style)
+#   end    
+# end
 
 
 # Fix for no method `fingerprint` in 3.0.2 / 3.0.3  >> https://github.com/thoughtbot/paperclip/issues/issue/346
-if defined? ActionDispatch::Http::UploadedFile
-  ActionDispatch::Http::UploadedFile.send(:include, Paperclip::Upfile)
-end
+# if defined? ActionDispatch::Http::UploadedFile
+#   ActionDispatch::Http::UploadedFile.send(:include, Paperclip::Upfile)
+# end
